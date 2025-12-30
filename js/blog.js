e043616b5d0e4e06ee7e8147155aaee1a547d292
Julius Markwick <julius.markwick@gmail.com>
2025-12-29 18:33:32 -0800

    sure

--- js/blog.js
+++ js/blog.js
@@ -1,27 +1,30 @@
-fetch("../assets/data/blog.json")
-    .then(response => response.json())
-    .then(blogs => {
-        blogs.sort((a,b) =>
-            new Date(b.date) - new Date(a.date));  // Sort by date
+const container = document.getElementById("blog-container");
+
+
+fetch("../../assets/data/blog.json")
+    .then(response => response.json());
+    // .then(blogs => {
+    //     blogs.sort((a,b) =>
+    //         new Date(b.date) - new Date(a.date));  // Sort by date
     
-        const container = document.getElementById("blog-container");
 
-        blogs.forEach(blog => {
+        let blogs = response.json()
+
+        for(let blog in blogs){
             const article = document.createElement("article");
-
-            if(blog.image==""){
-                article.innerHTML = `
+            article.innerHTML = `
                 <h2 class="blogTitle">${blog.title}</h2>
                 <h3 class="blogDate">${blog.date}</h3>
-                <p class="blogContent">${blog.content}</p>
-                <img class="blogImage" src="${blog.image}"/>`
-            } else if(blog.image==null){
-                article.innerHTML = `
-                <h2 class="blogTitle">${blog.title}</h2>
-                <h3 class="blogDate">${blog.date}</h3>
-                <p class="blogContent">${blog.content}</p>`
-            }
+                <p class="blogContent">${blog.content}</p>`;
+            container.appendChild(article);
+        }
 
-        container.appendChild(article);
-        });
-    });
+        // blogs.forEach(blog => {
+            // const article = document.createElement("article");
+
+            
+                
+
+        
+        // });
+    // });
