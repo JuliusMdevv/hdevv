238164e26e512fd47a459d0257d0d1f13b5b436a
Julius Markwick <julius.markwick@gmail.com>
2025-12-30 20:14:19 -0800

    OKAY IM CRAZY??

--- js/ham.js
+++ js/ham.js
@@ -5,7 +5,7 @@ const filename = window.location.pathname.split("/").pop();
 
 const test = document.createElement("test");
 test.innerHTML= `
-<h1>${filename}</h1>
+<h1>${filename.replace(".html", "")}</h1>
 `;
 
 offScreen.appendChild(test);
